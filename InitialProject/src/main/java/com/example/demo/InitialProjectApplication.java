28b75638f7e0ddbb9f4bf011f06989be3902a862
jimit.arun.bhatt <jimit.arun.bhatt@P4ALAP-9BZHX93.dir.svc.accenture.com>
2021-04-29 12:23:44 +0530

    This is commit 2

--- InitialProject/src/main/java/com/example/demo/InitialProjectApplication.java
+++ InitialProject/src/main/java/com/example/demo/InitialProjectApplication.java
@@ -9,6 +9,7 @@ public class InitialProjectApplication {
 	public static void main(String[] args) {
 		System.out.println("The application is running fine...");
 		System.out.println("This is the change in the file to commit...");
+		System.out.println("This is the change in the file to commit2...");
 		SpringApplication.run(InitialProjectApplication.class, args);
 	}
 		
