1d2da9d3bb83ad62ca8ed0d8b61679a17ef47dc2
jimit.arun.bhatt <jimit.arun.bhatt@P4ALAP-9BZHX93.dir.svc.accenture.com>
2021-04-30 13:26:08 +0530

    Branch Commit

--- InitialProject/src/main/java/com/example/demo/InitialProjectApplication.java
+++ InitialProject/src/main/java/com/example/demo/InitialProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InitialProjectApplication {
-
+//This is the first change done one the branch
 	public static void main(String[] args) {
 		System.out.println("The application is running fine...");
 		System.out.println("This is the change in the file to commit...");
